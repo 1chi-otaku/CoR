a12aa7f621dc2bfb80723ad40203afef9c66a00d
Serhii <mywaifuqueen@gmail.com>
2023-03-29 11:03:12 +0100

    Add project files.

---
+++ CoR/CoR.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoR
+{
+    abstract class PaymentHandler
+    {
+        public PaymentHandler Next;
+        public abstract void Handle(Receiver receiver);
+    }
+
+    class BankPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.BankTransfer)
+                Console.WriteLine("Bank Transfer");
+            else if (Next != null)
+                Next.Handle(receiver);
+        }
+    }
+
+    class MoneyPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.MoneyTransfer)
+                Console.WriteLine("Transfer through money transfer systems");
+            else if (Next != null)
+                Next.Handle(receiver);
+        }
+    }
+
+    class PayPalPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.PayPalTransfer)
+                Console.WriteLine("Transfer via paypal");
+            else if (Next != null)
+                Next.Handle(receiver);
+        }
+    }
+}
---
+++ CoR/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoR
+{
+    class Program
+    {
+        public static void Requst(PaymentHandler h, Receiver receiver)
+        {
+            h.Handle(receiver);
+        }
+        static void Main(string[] args)
+        {
+            PaymentHandler bankPaymentHandler = new BankPaymentHandler();
+            PaymentHandler moneyPaymentHandler = new MoneyPaymentHandler();
+            PaymentHandler paypalPaymentHandler = new PayPalPaymentHandler();
+
+            bankPaymentHandler.Next = paypalPaymentHandler;
+            paypalPaymentHandler.Next = moneyPaymentHandler;
+
+            Receiver receiver_1 = new Receiver(true, false,false);
+            Receiver receiver_2 = new Receiver(false, true, false);
+            Receiver receiver_3 = new Receiver(false, false, true);
+
+            Requst(bankPaymentHandler, receiver_1);
+            Requst(bankPaymentHandler, receiver_2);
+            Requst(bankPaymentHandler, receiver_3);
+        }
+    }
+}
